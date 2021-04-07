850baa0f6bd13eaff6a9a43e588c1e5eff7802f5
Evgeniy Sklyarov <skevni@gmail.com>
2021-04-08 01:12:10 +0300

    Race
    my changes

--- src/main/java/ru/geekbrains/sklyarov/race/Car.java
+++ src/main/java/ru/geekbrains/sklyarov/race/Car.java
@@ -11,29 +11,71 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private final CyclicBarrier cyclicBarrier;
+    // Когда я использовал в качестве монитора обертку Boolean без final, идея меня предупреждала, что так лучше не делать
+    // поэтому сделал мониторы и переменные
+    private static boolean isWinner = false;
+    private static boolean isOutLine = false;
+    private final static Boolean monWinner = false;
+    private final static Boolean monOutLine = false;
+
+    private final CountDownLatch countDownLatch;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier, CountDownLatch countDownLatch) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+        this.countDownLatch = countDownLatch;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            // ждем всех
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        synchronized (monOutLine) {
+            if (!isOutLine) {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                isOutLine = true;
+            }
+        }
         for (int i = 0; i < race.getStages().size(); i++) {
+
             race.getStages().get(i).go(this);
+
+            synchronized (monWinner) {
+                if (i == race.getStages().size() - 1) {
+                    if (!isWinner) {
+                        System.err.println(name + " - WIN ---------------------");
+                        isWinner = true;
+                    }
+                    countDownLatch.countDown();
+
+//                    CARS_COUNT--;
+                }
+//                if (CARS_COUNT == 0) {
+//                    System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+//                    synchronized (MainClass.lock) {
+//                        MainClass.lock.notifyAll();
+//                    }
+//                }
+            }
         }
     }
 }
--- src/main/java/ru/geekbrains/sklyarov/race/MainClass.java
+++ src/main/java/ru/geekbrains/sklyarov/race/MainClass.java
@@ -5,18 +5,34 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 
 public class MainClass {
-    public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+
+    // Количество участников
+    private static final int CARS_COUNT_MAX = 4;
+
+    // Барьер для старта
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT_MAX);
+    // Семафор для тоннеля
+    private final static  Semaphore semaphore = new Semaphore(2);
+    // сначала сделал через монитор, потом переделал на CountDownLatch
+    private final static CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT_MAX);
+
+    public static void main(String[] args) throws InterruptedException {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
+        Car[] cars = new Car[CARS_COUNT_MAX];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, countDownLatch);
         }
+
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+//        synchronized (lock) {
+//            lock.wait();
+//        }
+        countDownLatch.await();
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
