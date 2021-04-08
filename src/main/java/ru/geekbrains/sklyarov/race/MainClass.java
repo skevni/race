8b24e41c6ebf7d312ca87177b4e51878a5381ef9
Evgeniy Sklyarov <skevni@gmail.com>
2021-04-08 19:15:11 +0300

    Race
    my changes

--- src/main/java/ru/geekbrains/sklyarov/race/MainClass.java
+++ src/main/java/ru/geekbrains/sklyarov/race/MainClass.java
@@ -7,20 +7,20 @@ import java.util.concurrent.Semaphore;
 public class MainClass {
 
     // Количество участников
-    private static final int CARS_COUNT_MAX = 4;
+    private static final int CARS_COUNT = 4;
 
     // Барьер для старта
-    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT_MAX);
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
     // Семафор для тоннеля
     private final static  Semaphore semaphore = new Semaphore(2);
     // сначала сделал через монитор, потом переделал на CountDownLatch
-    private final static CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT_MAX);
+    private final static CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
 
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
-        Car[] cars = new Car[CARS_COUNT_MAX];
+        Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, countDownLatch);
         }
